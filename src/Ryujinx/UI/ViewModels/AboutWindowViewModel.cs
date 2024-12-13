b5ae940a8b42ca6cfa43ef7ab6a6c93380d02c34
suljarock <83973463+suljarock@users.noreply.github.com>
2024-12-13 07:20:35 -0800

    Add files via upload

--- src/Ryujinx/UI/ViewModels/AboutWindowViewModel.cs
+++ src/Ryujinx/UI/ViewModels/AboutWindowViewModel.cs
@@ -45,7 +45,7 @@ namespace Ryujinx.Ava.UI.ViewModels
             }
         }
 
-        public string Developers => "GreemDev";
+        public string Developers => "Sulja Rock";
 
         public string FormerDevelopers => LocaleManager.Instance.UpdateAndGetDynamicValue(LocaleKeys.AboutPageDeveloperListMore, "gdkchan, Ac_K, marysaka, rip in peri peri, LDj3SNuD, emmaus, Thealexbarney, GoffyDude, TSRBerry, IsaacMarovitz");
 
